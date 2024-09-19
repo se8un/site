876d88f0614b85ba252cb52091cc7ee611a42261
se8un <se8un@yandex.ru>
2024-09-19 13:44:44 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-document.addEventListener('mousemove', e => {
-	Object.assign(document.documentElement, {
-		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
-		`
-	})
-})
+document.addEventListener("mousemove", (e) => {
+  Object.assign(document.documentElement, {
+    style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.003}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * 0.001}deg;
+		`,
+  });
+});
